cd5bc1e6f2611a885485e305af1bc7eeb2d3f8bf
BenGump <74874223+BenGump@users.noreply.github.com>
2024-11-28 20:05:10 +0100

    WebGL Build

--- Assets/Scripts/PlayerAnimationHelper.cs
+++ Assets/Scripts/PlayerAnimationHelper.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAnimationHelper : MonoBehaviour
 {
@@ -27,4 +28,6 @@ public class PlayerAnimationHelper : MonoBehaviour
             npc.EndHarvestAnimation();
         }
     }
+
+    
 }
